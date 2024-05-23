75d05b2cd9efb856dee28e9d0c55b9df295a68a6
ciwaf1582 <ciwaf1582@gmail.com>
2024-05-23 15:54:19 +0900

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace 고급_컨트롤
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnlnsert = new System.Windows.Forms.Button();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbPhone = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbOrg = new System.Windows.Forms.TextBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.chName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chPhone = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chOrg = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(121, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "이름 :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("굴림", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(121, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 24);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "휴대폰 :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("굴림", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(121, 257);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 24);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "소속 :";
+            // 
+            // btnlnsert
+            // 
+            this.btnlnsert.Location = new System.Drawing.Point(474, 82);
+            this.btnlnsert.Name = "btnlnsert";
+            this.btnlnsert.Size = new System.Drawing.Size(222, 91);
+            this.btnlnsert.TabIndex = 1;
+            this.btnlnsert.Text = "입력";
+            this.btnlnsert.UseVisualStyleBackColor = true;
+            this.btnlnsert.Click += new System.EventHandler(this.btnlnsert_Click);
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(221, 82);
+            this.tbName.Multiline = true;
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(206, 46);
+            this.tbName.TabIndex = 2;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chName,
+            this.chPhone,
+            this.chOrg});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(125, 330);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(571, 259);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            
+            // 
+            // tbPhone
+            // 
+            this.tbPhone.Location = new System.Drawing.Point(221, 163);
+            this.tbPhone.Multiline = true;
+            this.tbPhone.Name = "tbPhone";
+            this.tbPhone.Size = new System.Drawing.Size(206, 46);
+            this.tbPhone.TabIndex = 2;
+           
+            // 
+            // tbOrg
+            // 
+            this.tbOrg.Location = new System.Drawing.Point(221, 247);
+            this.tbOrg.Multiline = true;
+            this.tbOrg.Name = "tbOrg";
+            this.tbOrg.Size = new System.Drawing.Size(206, 46);
+            this.tbOrg.TabIndex = 2;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(474, 202);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(222, 91);
+            this.btnDelete.TabIndex = 1;
+            this.btnDelete.Text = "삭제";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // chName
+            // 
+            this.chName.Text = "이름";
+            this.chName.Width = 230;
+            // 
+            // chPhone
+            // 
+            this.chPhone.Text = "휴대폰";
+            this.chPhone.Width = 171;
+            // 
+            // chOrg
+            // 
+            this.chOrg.Text = "소속";
+            this.chOrg.Width = 170;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 628);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.tbOrg);
+            this.Controls.Add(this.tbPhone);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnlnsert);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnlnsert;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbPhone;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbOrg;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.ColumnHeader chName;
+        private System.Windows.Forms.ColumnHeader chPhone;
+        private System.Windows.Forms.ColumnHeader chOrg;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 고급_컨트롤
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnlnsert_Click(object sender, EventArgs e)
+        {
+            if (tbName.Text == "" || tbPhone.Text == "" || tbOrg.Text == "")
+            {
+                MessageBox.Show("입력하지 않은 곳을 채워주세요");
+            }
+            else
+            {
+                string[] strArray = new string[] { tbName.Text, tbPhone.Text, tbOrg.Text };
+                ListViewItem lvt = new ListViewItem(strArray);
+                listView1.Items.Add(lvt);
+                tbName.Clear();
+                tbPhone.Clear();
+                tbOrg.Clear();
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int selectedIndex = listView1.FocusedItem.Index;
+            listView1.Items.RemoveAt(selectedIndex);
+        }
+    }
+}
